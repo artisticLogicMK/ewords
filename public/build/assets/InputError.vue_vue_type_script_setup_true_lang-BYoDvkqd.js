8c1630b9d1c5021863b6584b704787a238ef2dd1
artisticLogicMK <max.2xtremes@gmail.com>
2025-06-21 16:06:54 +0100

    ...

---
+++ public/build/assets/InputError.vue_vue_type_script_setup_true_lang-BYoDvkqd.js
@@ -0,0 +1,1 @@
+import{d as s,E as t,bK as a,o as r,a as o,e as n,t as p}from"./app-BvsTm2UU.js";const m={class:"text-sm text-red-600 dark:text-red-500 mt-2"},d=s({__name:"InputError",props:{message:{}},setup(c){return(e,i)=>t((r(),o("div",null,[n("p",m,p(e.message),1)],512)),[[a,e.message]])}});export{d as _};
